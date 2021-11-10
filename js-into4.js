f2bd91c2ad6b8dc523f2cf119008735452a6a30e
invincibleslugheads <G0015750@stu.raritanval.edu>
2021-11-10 18:32:48 -0500

    Completed assignments up to difficult. Did not run.

--- js-into4.js
+++ js-into4.js
@@ -7,30 +7,72 @@
 
 // given the string inputs '5' & '6' add them together so that they equal 11
 
+
+let strFive = "5";
+let strSix = "6";
+
+let numFive = +strFive;
+let numSix = +strSix;
+
+console.log(numFive + numSix);
+
 // convert the number 56 into a string
 
+(56).toString();
+
 // Using the '+' operator concatinate the following strings 'I', 'am', 'happy' 
+
+let strI = "I"
+let strAm = "am"
+let strHappy = "happy"
+
+console.log("strI + strAm + strHappy")
 
 // Given the variables below 
 // Create a sentence by concatinating them with template literals
 // Log that sentence to the console
 
-const adjective;
-const verb;
-const noun;
+const adjective = "cautious";
+const verb = "bake";
+const noun = "Australia";
+
+
+const tempLiteralSent = "I am ${adjective} when I ${verb} for ${Australia}.";
+
+console.log(tempLiteralSent);
 
 // Given the following statement, fix it so that it logs to the console properly:
-// console.log('These are Cookie Monster's cookies!')
+
+console.log("These are Cookie Monster's cookies!");
+
 
 // Given the following statement, fix it so that it logs to the console properly:
 // Do so by utilizing the escape backslash
-// console.log('Those are Harry's glasses')
+
+console.log('Those are Harry\'s glasses');
 
 // Given the following names get the initials:
 const name1 = 'Harry Potter';
 const name2 = 'Jack Sparrow';
 const name3 = 'Peter Jackson';
 const name4 = 'Samwise Gamgee ';
+
+let name1 = alph[0] + alph[6];
+
+let name2 = alph[0] + alph[5];
+
+let name3 = alph[0] + alph[6];
+
+let name4 = alph[0] + alph[8];
+
+
+console.log(name1);
+
+console.log(name2);
+
+console.log(name3);
+
+console.log(name4);
 
 // DIFFICULT:
 
