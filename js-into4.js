fa005e68bf7753a54d9f66d412c8cb2efd0120d5
invincibleslugheads <G0015750@stu.raritanval.edu>
2021-11-09 20:43:48 -0500

    Added instruction to commit

---
+++ js-into4.js
@@ -0,0 +1,40 @@
+// Push this to GitHub run the three lines
+// git add .
+//git commit -m"Custom message here"
+//git push
+
+
+
+// given the string inputs '5' & '6' add them together so that they equal 11
+
+// convert the number 56 into a string
+
+// Using the '+' operator concatinate the following strings 'I', 'am', 'happy' 
+
+// Given the variables below 
+// Create a sentence by concatinating them with template literals
+// Log that sentence to the console
+
+const adjective;
+const verb;
+const noun;
+
+// Given the following statement, fix it so that it logs to the console properly:
+// console.log('These are Cookie Monster's cookies!')
+
+// Given the following statement, fix it so that it logs to the console properly:
+// Do so by utilizing the escape backslash
+// console.log('Those are Harry's glasses')
+
+// Given the following names get the initials:
+const name1 = 'Harry Potter';
+const name2 = 'Jack Sparrow';
+const name3 = 'Peter Jackson';
+const name4 = 'Samwise Gamgee ';
+
+// DIFFICULT:
+
+// If possible, Without referring to the notes, capture the last character of a string
+
+//Bonus: Dynamically get the initials from the above names
+
---
+++ js-practice.js
@@ -0,0 +1,101 @@
+// /*
+
+//   Create a .js file that satisfies assignment below
+
+//   For each exercise you'll want to log the result to the console to check your work
+
+// */  
+
+ 
+
+//   // 1. Declare a new variable and set it equal to the string of your name
+
+//       let firstName = "Thomas";
+//       console.log(firstName)
+
+   
+
+ 
+
+//   // 2. Declare a new variable and set it equal to a number
+
+//         let numDay = 3
+//         console.log(numDay)
+
+        
+ 
+
+   
+
+//   // 3. Set a variable to a boolean value
+
+ 
+//         let night = true;
+//         console.log(night)
+
+
+ 
+
+//   // 4. Create a variable called carName, assign the value Volvo to it.
+
+//         let carName = "Volvo";
+//         console.log(carName)
+
+ 
+
+//   // 5. Create a variable called num, assign the value 50 to it.
+
+//           let num = 50;
+//           console.log(num)
+ 
+
+ 
+
+//   // 6. Display the sum of 5 + 10, using two variables: num1 and num2.
+
+//         let num1 = 5;
+//         let num2 = 10;
+
+//       console.log(num1 + num2)
+
+   
+
+ 
+
+//   // 7. Create a variable called num3, assign the addition of num1 and num2 to it.
+
+//         let num3 = (num1 + num2);
+//         console.log(num3)
+
+//         // Create two variables, with the two modern variable assignment keywords.
+
+//             let num4 = 20;
+//             const birthday = 3;
+//             console.log(num4)
+//             console.log(birthday)
+
+
+
+
+//         // Try and re-assign the value for each. Observe which one works and which doesn't
+
+
+//               num4 = 40;
+//               console.log(num4) // this before prints before decleration
+//               birthday = 13;
+//               console.log(birthday) //throws error cannont reasgin const
+
+
+
+const alph = "abcdefghijklmnopqrstuvwxyz";
+
+let name = alph[19] + alph[14] + alph[12]
+
+
+console.log(name);
+
+// let city = "New York";
+
+// city[0] //this will get us "N"
+// city[2] //this will get us "w"
+// city[3] // this will get us the space
