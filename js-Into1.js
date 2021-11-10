ec14b2c015baf64b4d5f015e2807c03b69da39eb
invincibleslugheads <G0015750@stu.raritanval.edu>
2021-11-09 19:42:29 -0500

    first commit

---
+++ js-Into1.js
@@ -0,0 +1,21 @@
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+
+let age = 13;
+let height = 17;
+let name = "Ricky";
+let weight = 26;
+let color = "tan";
+let animalType = "Dog";
+
+console.log(name + " is a " + color + " colored " + animalType + " who is " + age + " years old and weights " + weight + "lbs and is " + height + " inches tall")
+
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+
+      let carModel = "Commander";
+      let carMake = "Jeep";
+      let carYear = 2008;
+      let carType = "SUV";
+      let carMpg = 13;
+      let carEngineSize = 5.7;
+
+      console.log(`I have a ${carYear} ${carMake} ${carModel} ${carType}, which gets ${carMpg} mpg with a ${carEngineSize} liter engine`)
