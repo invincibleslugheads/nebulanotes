09b27bd20e9d18df97acc52abdf850b337bb7118
invincibleslugheads <G0015750@stu.raritanval.edu>
2021-11-16 18:28:16 -0500

    Completed all assignments prior to 11/10

--- js-Into2.js
+++ js-Into2.js
@@ -8,7 +8,13 @@
 // Extract the word 'academy' and log it
 
 
+const sayJob = "We are software engineers at Nebula Academy";
 
+console.log(sayJob.indexOf("software"));  // Log the index of 'software'
+console.log(sayJob.indexOf("s"));  // Log the index of 's'
+console.log(sayJob[0] + sayJob[1] + sayJob[31]);  // Log the index of 'web'
+console.log(sayJob.slice(7, 14));  // Extract the word 'software' and log it
+console.log(sayJob.slice(36, 42));  // Extract the word 'academy' and log it
 
 
 // Create a variable and assign the string: "we are learning new string methods"
@@ -21,13 +27,21 @@
 // Lowercase everything in the string
 
 
+const student = "We are learning new string methods";
 
+console.log(student.length)  // Log the length of the string
+console.log(student[student.length-1]);  // Log the last character of the string
+console.log(student.indexOf("e"))  // Log the character at index 5
+console.log(student.toUpperCase());  // Uppercase everything in the string
+console.log(student.toLowerCase());  // Lowercase everything in the string
 
 
 // Create a new variable with your favorite ice cream flavor
 // Log the string, 'My favorite ice cream flavor is (your favorite ice cream flavor)'
 
+const favIceCream = "pistachio";
 
+console.log('My favorite ice cream flavor is  ${favIceCream}')
 
 
 // After completing push your file to github:
--- js-basics/script.js
+++ js-basics/script.js
@@ -1,1 +1,6 @@
 console.log("Hello World")
+
+
+let city = 'New York';
+let lastChar = city[city.length-1];
+console.log(lastChar);
